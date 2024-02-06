5da4761bfde421485499860d10492749108329b9
Elyticus <125762622+Elyticus@users.noreply.github.com>
2024-02-06 20:58:19 +0200

    fixed

--- card-app.js
+++ card-app.js
@@ -1,35 +1,142 @@
-const cardHolder = document.getElementById("input__name");
-const cardNumber = document.getElementById("input__card");
-const expMonth = document.getElementById("input__month");
-const expYear = document.getElementById("input__year");
-const cvcNumber = document.getElementById("input__cvc");
-
-const displayName = document.getElementById("display__name");
-const displayExpDate = document.getElementById("display__exp__date");
-const displayCard = document.getElementById("display__card");
-const displayYear = document.getElementById("display__exp__year");
-const displayCVC = document.getElementById("display__cvc");
-
-const cardInfo = document.querySelector(".card__info");
-const infoSection = document.querySelector(".final__section");
-const finalButtonElement = document.querySelector(".final__button");
-
+const inputs = document.querySelectorAll("input");
 const confirmButton = document.getElementById("confirm__btn");
 
-function handleInputElements() {
-  const displayError = document.querySelectorAll(".blank__class")[0];
+const cardInfos = document.querySelectorAll(
+  "#input__name, #input__card, #input__month, #input__year, #input__cvc"
+);
 
-  const regexLetter = /[a-zA-Z]+/;
+const errorElement = document.querySelectorAll(".error-message");
+const date = new Date();
 
-  if (cardHolder.value.trim() === "") {
-    // cardHolder.classList.add("redError");
-    displayError.style.display = "block";
-    displayError.textContent = "Can't be blank!";
-  } else if (cardHolder.value !== regexLetter) {
-    displayError.textContent = "Invalid characters!";
-  } else {
-    displayError.textContent = "Your registration name has accepted.";
+const handleConfirmation = (value) => {
+  const formContainer = document.querySelector(".card__info");
+  const completeState = document.querySelector(".final__section");
+
+  if (value) {
+    formContainer.classList.toggle("display");
+    completeState.classList.toggle("display");
   }
-}
+};
 
-confirmButton.addEventListener("click", handleInputElements);
+const checkInputs = (data) => {
+  const year = date.getFullYear().toString().slice(2);
+  const yearInt = parseInt(year);
+  let isValid = true;
+
+  errorElement.forEach((e, index) => {
+    if (e.classList.contains(data[index].name) && data[index].value === "") {
+      e.classList.add("display");
+      inputs[index].classList.add("error-input");
+    } else if (
+      e.classList.contains(data[index].name) &&
+      data[index].value !== ""
+    ) {
+      e.classList.remove("display");
+      inputs[index].classList.remove("error-input");
+
+      if (data[index].name === "input__card") {
+        if (data[index].value.length < 16) {
+          e.innerHTML = "please insert a card number";
+          e.classList.add("display");
+          inputs[index].classList.add("error-input");
+          isValid = false;
+          return;
+        }
+      } else if (data[index].name === "input__year") {
+        if (
+          parseInt(data[index].value) < yearInt ||
+          parseInt(data[index].value) > yearInt + 10
+        ) {
+          e.innerHTML = "please insert a valid year";
+          e.classList.add("display");
+          inputs[index].classList.add("error-input");
+          isValid = false;
+          return;
+        }
+      } else if (data[index].name === "input__month") {
+        if (
+          parseInt(data[index].value) < 1 ||
+          parseInt(data[index].value) > 12
+        ) {
+          e.innerHTML = "please insert a valid month";
+          e.classList.add("display");
+          inputs[index].classList.add("error-input");
+          isValid = false;
+          return;
+        }
+      } else if (data[index].name === "input__cvc") {
+        if (data[index].value.length < 3) {
+          e.innerHTML = "please insert a valid cvc";
+          e.classList.add("display");
+          inputs[index].classList.add("error-input");
+          isValid = false;
+          return;
+        }
+      }
+    }
+
+    if (data[index].value === "") {
+      isValid = false;
+    }
+  });
+  handleConfirmation(isValid);
+};
+
+const handleButton = (event) => {
+  event.preventDefault();
+
+  let data = [];
+  for (let input of inputs) {
+    data.push({ name: input.id, value: input.value });
+  }
+
+  checkInputs(data);
+};
+
+const formatCardNumber = (value) => {
+  let result = "";
+  for (let i = 0; i < value.length; i++) {
+    result += value[i];
+    if ((i + 1) % 4 === 0 && i !== value.length - 1) {
+      result += " ";
+    }
+  }
+  return result;
+};
+
+const handleCardInfoUpdate = () => {
+  for (let input of inputs) {
+    for (let infos of cardInfos) {
+      if (input.id === infos.classList.value) {
+        input.addEventListener("input", () => {
+          infos.innerHTML = `${
+            input.id === "input__card"
+              ? formatCardNumber(input.value)
+              : input.value
+          }`;
+          input.value = `${input.value}`;
+        });
+      }
+    }
+  }
+};
+
+const reload = () => {
+  location.reload();
+};
+
+const handleKeyDown = (event) => {
+  const { key } = event;
+
+  if (event.target.id !== "input__name") {
+    if (!/^\d$/.test(key) && key !== "Backspace") {
+      event.preventDefault();
+    } else {
+      handleCardInfoUpdate();
+    }
+  } else {
+    handleCardInfoUpdate();
+  }
+};
+
+confirmButton.addEventListener("click", checkInputs);
